9c41a15a1f5af4b0c09f4b3699968bce0f050cd2
Jiban Adhikary <adhikaryjiban@gmail.com>
2019-02-10 15:09:17 -0500

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,57 +1,61 @@
 #!/usr/bin/python
 
-import kenlm  # pip install https://github.com/kdv123/archive/master.zip
+import os
+import kenlm  # pip install https://github.com/kdv123/kenlm/archive/master.zip
 import vocabtrie
-
-INF = 2147483647
-
 class WordPredictor:
     wordList = []
 
     def __init__(self, lm_filename, vocab_filename):
-        self.lmFileName = lm_filename
+        self.lm_filename = lm_filename
         self.vocab_filename = vocab_filename
         self.language_model = kenlm.LanguageModel(lm_filename)
-        #char_list can also be constructed by calling the create_char_list_from_vocab function
-        #To create a character list when creating a trie call the above mentioned function
-        #from add_vocab function
-        self.next_char_li = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                             's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', "'", '.', ',', '?', '#', '$', '@']
-        self.trieTable = {}
-        self.add_vocab('', self.vocab_filename)
+
+        self.char_list = self.create_char_list_from_vocab('', vocab_filename)
+        #self.next_char_li = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                             #'s', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', "'", '.', ',', '?', '#', '$', '@']
+        self.trie_table = {}
+        self.trie_table[''] = self.create_new_trie(vocab_filename)
 
     # Given a filename, this method creates a new trie data
     # structure for the words in the file
-    @staticmethod
-    def create_new_trie(vocab_filename):
+    def create_new_trie(self, vocab_filename):
         word_list = []
-        newVocabTrie = vocabtrie.VocabTrie()
-        with open(vocab_filename) as fp:
-            for line in fp:
-                line = line.strip()
-                word_list.append(line)
-                newVocabTrie.add_word(line)
-        return newVocabTrie
+        new_vocab_trie = vocabtrie.VocabTrie()
+        if os.path.exists(vocab_filename):
+            with open(vocab_filename) as fp:
+                try:
+                    for line in fp:
+                        line = line.strip()
+                        word_list.append(line)
+                        new_vocab_trie.add_word(line)
+                except IOError:
+                    print('Vocab file does not exist.')
+        return new_vocab_trie
 
-    #Function to update character list
+    # Function to update character list
     @staticmethod
     def update_char_list_from_string(char_list, string):
         for char in string:
             char_list.append(char)
         return char_list
 
-    #Given a vocab_id and vocab_filename, the following function adds every
-    #unique character of the vocabulary in a character list
-    @staticmethod
-    def create_char_list_from_vocab(vocab_id, vocab_filename):
+    # Given a vocab_id and vocab_filename, the following function adds every
+    # unique character of the vocabulary in a character list
+    def create_char_list_from_vocab(self, vocab_id, vocab_filename):
         char_list = set()
         char_list_by_vocab_id = {}
-        with open(vocab_filename) as fp:
-            #i = 0
-            for line in fp:
-                line = line.strip()
-                for char in line:
-                    char_list.add(char)
+        if os.path.exists(vocab_filename):
+            with open(vocab_filename) as fp:
+                try:
+
+                    #i = 0
+                    for line in fp:
+                        line = line.strip()
+                        for char in line:
+                            char_list.add(char)
+                except IOError:
+                    print('Vocab file does not exist.')
 
         char_list_by_vocab_id[vocab_id] = char_list
         return char_list_by_vocab_id
@@ -62,29 +66,29 @@ class WordPredictor:
             return ',comma'
         elif punctuation == '.':
             return '.period'
-        elif punctuation == ',?':
+        elif punctuation == '?':
             return '?question-mark'
         elif punctuation == '!':
             return '!exclamation-point'
 
     # This method adds a vocabulary to the instance of the class. The vocabulary
     # is saved as a Trie data structure. For multiple vocabularies, the Tries are
-    # mapped in a hash table with a vocab_id
+    # mapped in a hash table with a vocab_id.
     # The trie can be accessed by the vocab_id provided
     def add_vocab(self, vocab_id, vocab_filename):
-        if self.trieTable.has_key(vocab_id):
+        if self.trie_table.has_key(vocab_id):
             print('Vocabulary with id "' + vocab_id + '" already exists, try a new id.')
             return False
         new_trie = self.create_new_trie(vocab_filename)
-        self.trieTable[vocab_id] = new_trie
+        self.trie_table[vocab_id] = new_trie
         print('Vocab added successfully')
         return True
 
     # Given a vocab_id, this method returns the trie from the trie table that
     # is referenced by the vocab_id
     def get_vocab_trie(self, vocab_id):
-        if self.trieTable.has_key(vocab_id):
-            return True, self.trieTable[vocab_id]
+        if self.trie_table.has_key(vocab_id):
+            return True, self.trie_table[vocab_id]
         else:
             #print('Error! Vocabulary with id "' + vocab_id + ' " has not been found.')
             return False, None
@@ -93,59 +97,29 @@ class WordPredictor:
 
     # This method returns the kenlm states for the given context
     def get_context_state(self, context, model):
-        stateIn = kenlm.State()
-        stateOut = kenlm.State()
+        state_in = kenlm.State()
+        state_out = kenlm.State()
         context = '<s> ' + context
         contextWords = context.split()
         for w in contextWords:
-            print('Context', '{0}\t{1}'.format(model.BaseScore(stateIn, w, stateOut), w))
-            stateIn = stateOut
-            stateOut = kenlm.State()
+            print('Context', '{0}\t{1}'.format(model.BaseScore(state_in, w, state_out), w))
+            state_in = state_out
+            state_out = kenlm.State()
 
-        return stateIn, stateOut
+        return state_in, state_out
 
-    def get_words(self, *args):
-        num_of_args = len(args)
-        prefix = ''
-        context = ''
-        vocab_id = ''
-        num_predictions = 3
-        min_log_prob = -INF
+    def get_words(self, prefix='', vocab_id='', num_predictions=0, min_log_prob= float('inf')):
+        return self._get_words(prefix, '', vocab_id, num_predictions, min_log_prob)
 
-        # arguments in the form 'prefix, vocab_id, num_of_predictions, min_log_prob'
-        if num_of_args == 4:
-            prefix = args[0]
-            # print(prefix)
-            vocab_id = args[1]
-            # print(vocab_id)
-            num_predictions = args[2]
-            # print(num_of_predictions)
-            min_log_prob = args[3]
-            # print(min_log_prob)
-            return self._get_words(prefix, '', vocab_id, num_predictions, min_log_prob)
 
-        # arguments in the form 'prefix, context, vocab_id, num_of_predictions, min_log_prob'
-        elif num_of_args == 5:
-            prefix = args[0]
-            # print(prefix)
-            context = args[1]
-            # print(context)
-            vocab_id = args[2]
-            # print(vocab_id)
-            num_of_predictions = args[3]
-            # print(num_of_predictions)
-            min_log_prob = args[4]
-            # print(min_log_prob)
-            return self._get_words(prefix, context, vocab_id, num_predictions, min_log_prob)
+    def get_words_with_context(self, prefix='', context='', vocab_id='', num_predictions=0, min_log_prob = float('inf')):
+        return self._get_words(prefix, context, vocab_id, num_predictions, min_log_prob)
 
-        else:
-            print('Invalid number of arguments')
-            return []
 
     def _get_words(self, prefix, context, vocab_id, num_predictions, min_log_prob):
-        (stateIn, stateOut) = self.get_context_state(context, self.language_model)
+        (state_in, state_out) = self.get_context_state(context, self.language_model)
 
-        flag, currentTrie = self.get_vocab_trie(vocab_id)
+        flag, current_trie = self.get_vocab_trie(vocab_id)
         if flag == False:
             return []
 
@@ -153,28 +127,32 @@ class WordPredictor:
         most_prob_word_log = min_log_prob
 
         suggestion_list = []
-        for char in self.next_char_li:
+        for char in self.char_list[vocab_id]:
             new_prefix = prefix + char
             # print(new_prefix)
-            words_with_log_prob = currentTrie.get_words_with_prefix(new_prefix, self.language_model, stateIn, stateOut)
+            words_with_logprob = current_trie.get_words_with_prefix(new_prefix, self.language_model, state_in, state_out)
 
-            #Update the most probable word
-            most_prob_word, most_prob_word_log = self.find_most_probable_word(words_with_log_prob, most_prob_word, most_prob_word_log)
+            #update the most probable word
+            most_prob_word, most_prob_word_log = self.find_most_probable_word(words_with_logprob, most_prob_word, most_prob_word_log)
 
             #sort the most probable words for this prefix
-            likely_words_with_logprob = sorted(words_with_log_prob, key=lambda x: float(x[1]), reverse=True)
+            likely_words_with_logprob = sorted(words_with_logprob, key=lambda x: float(x[1]), reverse=True)
 
             #add the most probable words to the suggestion list for this prefix
-            suggestion_list.append(likely_words_with_logprob[:num_predictions])
+            if num_predictions < 1:
+                suggestion_list.append(likely_words_with_logprob)
+            else:
+                suggestion_list.append(likely_words_with_logprob[:num_predictions])
+
 
         # Print the most probable word if needed
-        #print('Context: ' + context)
-        #print('Prefix: ' + prefix)
-        #print('Most likely word: "' + most_prob_word + '" with log probability: ' + str(most_prob_word_log))
+        # print('Context: ' + context)
+        # print('Prefix: ' + prefix)
+        # print('Most likely word: "' + most_prob_word + '" with log probability: ' + str(most_prob_word_log))
         return suggestion_list
 
-    #Input: a list of probable words and their proability for a list of prefix
-    #Example: [[0, [['a', -2.04],['aa', -3.04],['ab', -2.04]]], [[1, [['b', -2.04],['ba', -3.04],['bb', -2.04]]] .....]
+    # Input: a list of probable words and their proability for a list of prefix
+    # Example: [[0, [['a', -2.04],['aa', -3.04],['ab', -2.04]]], [[1, [['b', -2.04],['ba', -3.04],['bb', -2.04]]] .....]
     def print_suggestions(self, suggestion_list):
         print('--------------------------------------------')
         print('Word\tProbability')
@@ -192,16 +170,16 @@ class WordPredictor:
                 log_prob = p
         return word, log_prob
 
-    def get_most_probable_word(self, prefix, context, vocab_id, min_log_prob = -INF):
-        (stateIn, stateOut) = self.get_context_state(context, self.language_model)
+    def get_most_probable_word(self, prefix, context, vocab_id, min_log_prob = -float('inf')):
+        (state_in, state_out) = self.get_context_state(context, self.language_model)
         most_prob_word = ''
         most_prob_word_log = min_log_prob
 
-        flag, currentTrie = self.get_vocab_trie(vocab_id)
+        flag, current_trie = self.get_vocab_trie(vocab_id)
         if flag == False:
             return most_prob_word, most_prob_word_log
 
-        words_with_log_prob = currentTrie.get_words_with_prefix(prefix, self.language_model, stateIn, stateOut)
+        words_with_log_prob = current_trie.get_words_with_prefix(prefix, self.language_model, state_in, state_out)
         # Update the most probable word
         most_prob_word, most_prob_word_log = self.find_most_probable_word(words_with_log_prob, most_prob_word, most_prob_word_log)
 
@@ -220,15 +198,15 @@ def main():
 
     #print(predictor.create_char_list_from_vocab(vocab_filename))
 
-    #words = predictor.get_words('f', '', 3, -inf)
+    #words = predictor.get_words('f', '', 3, -float('inf')
     #predictor.print_suggestions(words)
 
-    words = predictor.get_words('a', 'the united states of', '', 3, -INF)
+    words = predictor.get_words_with_context('a', 'the united states of', '', 2, -float('inf'))
     predictor.print_suggestions(words)
     #print(predictor.get_most_likely_word(words))
     #predictor.add_vocab('vocab_100k', vocab_filename)
 
-    predictor.get_most_probable_word('a', 'the united states of', '')
+    #predictor.get_most_probable_word('a', 'the united states of', '')
 
 
 
